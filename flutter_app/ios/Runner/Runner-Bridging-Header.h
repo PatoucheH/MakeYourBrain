7c0d3ad39ef300436a421c585193a2fe4b4eef7d
PatoucheH <hugopat@live.be>
2026-03-06 19:23:20 +0100

    retry

--- flutter_app/ios/Runner/Runner-Bridging-Header.h
+++ flutter_app/ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,28 @@
 #import "GeneratedPluginRegistrant.h"
+
+// Individual plugin headers for manual registration in AppDelegate.swift.
+// shared_preferences_foundation is intentionally excluded — crashes on iOS 26.
+#if __has_include(<app_links/AppLinksIosPlugin.h>)
+#import <app_links/AppLinksIosPlugin.h>
+#endif
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#endif
+#if __has_include(<firebase_messaging/FLTFirebaseMessagingPlugin.h>)
+#import <firebase_messaging/FLTFirebaseMessagingPlugin.h>
+#endif
+#if __has_include(<google_mobile_ads/FLTGoogleMobileAdsPlugin.h>)
+#import <google_mobile_ads/FLTGoogleMobileAdsPlugin.h>
+#endif
+#if __has_include(<google_sign_in_ios/FLTGoogleSignInPlugin.h>)
+#import <google_sign_in_ios/FLTGoogleSignInPlugin.h>
+#endif
+#if __has_include(<sign_in_with_apple/SignInWithApplePlugin.h>)
+#import <sign_in_with_apple/SignInWithApplePlugin.h>
+#endif
+#if __has_include(<url_launcher_ios/URLLauncherPlugin.h>)
+#import <url_launcher_ios/URLLauncherPlugin.h>
+#endif
+#if __has_include(<webview_flutter_wkwebview/WebViewFlutterPlugin.h>)
+#import <webview_flutter_wkwebview/WebViewFlutterPlugin.h>
+#endif
